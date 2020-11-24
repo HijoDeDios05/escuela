a47b766c4a95d517547b351f1ed3c610cd2250ee
Adolfo Herrera <jahf_94@live.com>
2020-11-24 16:24:37 -0600

    Terminado el estudiantemodel

--- app/Models/EstudianteModel.php
+++ app/Models/EstudianteModel.php
@@ -13,6 +13,14 @@ class EstudianteModel extends Model
 
    protected $validationRules = [
        'nombre' => 'required|alpha_space|min_length[3]|max_length[75]',
+       'apellido' => 'required|alpha_space|min_length[3]|max_length[75]',
+       'genero' => 'required|alpha_space|min_length[1]|max_length[1]',
+       'carnet' => 'required|alpha_space|min_length[9]|max_length[9]',
+       'dui' => 'required|alpha_space|min_length[9]|max_length[9]',
    ];
+   protected $validationMessages = [
+   
+];
+protected $skipValidation = false;
 
 }
