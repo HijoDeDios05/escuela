3783ef3ae2e338798e3466bb60a7142fe956c684
Adolfo Herrera <jahf_94@live.com>
2020-11-27 19:40:22 -0600

    Subiendo las validation rules

--- app/Models/EstudianteModel.php
+++ app/Models/EstudianteModel.php
@@ -12,14 +12,42 @@ class EstudianteModel extends Model
    protected $updatedField = 'updated_at';
 
    protected $validationRules = [
-       'nombre' => 'required|alpha_space|min_length[3]|max_length[75]',
-       'apellido' => 'required|alpha_space|min_length[3]|max_length[75]',
-       'genero' => 'required|alpha_space|min_length[1]|max_length[1]',
-       'carnet' => 'required|alpha_space|min_length[9]|max_length[9]',
-       'dui' => 'required|alpha_space|min_length[9]|max_length[9]',
+    'nombre'    => 'required|alpha_space|min_length[3]|max_length[75]',
+    'apellido'  => 'required|alpha_space|min_length[3]|max_length[75]',
+    'dui'       => 'required|regex_match[^\\d{8}-\\d$]',
+    'genero'    => 'required|regex_match[M|F|m|f]',
+    'carnet'    => 'required|regex_match[^\\u|U\\d{8}$]',
+    'grado_id'  => 'required',
    ];
    protected $validationMessages = [
-   
+    'nombre' => [
+        'alpha_space' => 'No se permiten numeros',
+        'required'    => 'Este campo no puede ir vacio',
+        'min_length'    => 'El minimo de letras es 3',
+        'max_length'    => 'El maximo de letras es 75',
+    ],
+
+    'apellido' => [
+        'alpha_space' => 'No se permiten numeros',
+        'required'    => 'Este campo no puede ir vacio',
+        'min_length'    => 'El minimo de letras es 3',
+        'max_length'    => 'El maximo de letras es 75',
+    ],
+
+    'dui' => [
+        'alpha_numeric_space' => 'Solo se permiten numeros',
+        'regex_match' => 'El formato es 00000000-0',
+    ],
+
+    'genero' => [
+        'required'    => 'Este campo no puede ir vacio',
+        'regex_match' => 'Solo se permite M o F',
+    ],
+
+    'carnet' => [
+        'required'    => 'Este campo no puede ir vacio',
+        'regex_match' => 'El formato es u20200000',
+    ],
 ];
 protected $skipValidation = false;
 
