f8ac4c99a7306759a755b9b4ceff6e96596746ab
Adolfo Herrera <jahf_94@live.com>
2020-11-27 20:31:02 -0600

    subiendo models

---
+++ app/Models/Profesormodel.php
@@ -0,0 +1,54 @@
+<?php namespace App\Models;
+use CodeIgniter\Model;
+
+class EstudianteModel extends Model
+{
+   protected $table = 'estudiante';
+   protected $primaryKey = 'id';
+   protected $returnType = 'array';
+   protected $allowedFields = ['nombre', 'apellido', 'genero', 'carnet', 'dui'];
+   protected $useTimetamps = true;
+   protected $createdField = 'created_at';
+   protected $updatedField = 'updated_at';
+
+   protected $validationRules = [
+    'nombre'    => 'required|alpha_space|min_length[3]|max_length[75]',
+    'apellido'  => 'required|alpha_space|min_length[3]|max_length[75]',
+    'dui'       => 'required|regex_match[^\\d{8}-\\d$]',
+    'genero'    => 'required|regex_match[M|F|m|f]',
+    'carnet'    => 'required|regex_match[^\\u|U\\d{8}$]',
+    'grado_id'  => 'required',
+   ];
+   protected $validationMessages = [
+    'nombre' => [
+        'alpha_space' => 'No se permiten numeros',
+        'required'    => 'Este campo no puede ir vacio',
+        'min_length'    => 'El minimo de letras es 3',
+        'max_length'    => 'El maximo de letras es 75',
+    ],
+
+    'apellido' => [
+        'alpha_space' => 'No se permiten numeros',
+        'required'    => 'Este campo no puede ir vacio',
+        'min_length'    => 'El minimo de letras es 3',
+        'max_length'    => 'El maximo de letras es 75',
+    ],
+
+    'dui' => [
+        'alpha_numeric_space' => 'Solo se permiten numeros',
+        'regex_match' => 'El formato es 00000000-0',
+    ],
+
+    'genero' => [
+        'required'    => 'Este campo no puede ir vacio',
+        'regex_match' => 'Solo se permite M o F',
+    ],
+
+    'carnet' => [
+        'required'    => 'Este campo no puede ir vacio',
+        'regex_match' => 'El formato es u20200000',
+    ],
+];
+protected $skipValidation = false;
+
+}
