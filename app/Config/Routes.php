e335953cfdc19d1a6eb2ed54cfe9505db5adbc1e
Adolfo Herrera <jahf_94@live.com>
2020-12-05 21:34:10 -0600

    subiendo filtros y su ruta

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->post('/auth/login', 'Auth::login');
-$routes->group('api',['namespace' => 'App\Controllers\API'], function($routes){
+$routes->group('api',['namespace' => 'App\Controllers\API','filter' => 'authFilter'], function($routes){
 	$routes->get('estudiantes', 'Estudiantes::index');
 	$routes->post('estudiantes/create', 'Estudiantes::create'); 
 	$routes->get('estudiantes/edit/(:num)', 'Estudiantes::edit/$1');
---
+++ app/Filters/AuthFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filters;
+
+use App\Models\RolModel;
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\API\ResponseTrait;
+use Config\Services;
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
+
+class AuthFilter implements FilterInterface {
+    use ResponseTrait;
+
+    public function before(RequestInterface $request, $arguments = null){
+        try {
+            
+            $key = Services::getSecretKey();
+            $authHeader = $request->getServer('HTTP_AUTHORIZATION');
+            if ($authHeader == null)
+                return Services::response()->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED, 'No se ha enviado el JWT');
+
+                $arr = explode(' ', $authHeader);
+                $jwt = $arr[1];
+                $jwt = JWT::decode($jwt, $key, ['HS256']);
+
+                $rolModel = new RolModel();
+                $rol = $rolModel->find($jwt->data->rol);
+
+                if ($rol)
+                return Services::response()->getStatusCode(ResponseInterface::HTTP_UNAUTHORIZED, 'El Rol del JWT es invalido');
+
+                return true;
+        }catch (ExpiredException $ee) {
+            return Services::response()->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED, 'Token caducado');
+        } 
+        catch (\Exception $e) {
+            return Services::response()->getStatusCode(ResponseInterface::HTTP_INTERNAL_SERVER_ERROR, 'Ha ocurrido un error en el servidor');
+        }
+    }
+
+	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null){
+        
+    }
+
+}
