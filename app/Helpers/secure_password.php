0e7bc9eadef231817a4278302b3ee8b4e746e2e4
Adolfo Herrera <jahf_94@live.com>
2020-12-05 20:46:26 -0600

    subiendo secure password modi

--- app/Helpers/secure_password.php
+++ app/Helpers/secure_password.php
@@ -2,4 +2,9 @@
 function hashPassword($plainText){
     return password_hash($plainText,PASSWORD_BCRYPT);
 }
+function verifyPassword($plainText, $hash)
+{
+    # code...
+   return password__verify($plainText,$hash);
+}
 ?>
